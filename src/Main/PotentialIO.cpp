3ef261c3072aa938952262b9d2ae076719ea6e55
Franco Moitzi <franco.moitzi@mcl.at>
2021-08-28 03:45:21 +0200

    feature(LSF): add lsf function to the local atom type

--- src/Main/PotentialIO.cpp
+++ src/Main/PotentialIO.cpp
@@ -146,8 +146,7 @@ int loadPotentials(LSMSCommunication &comm,
         local.atom[i].kkrsz = (local.atom[i].lmax + 1) *
                               (local.atom[i].lmax + 1);
     // LSF
-    auto lsf_functional = crystal.types[local.global_id[i]].lsf_functional;
-
+      auto lsf_functional = crystal.types[local.global_id[i]].lsf_functional;
       local.atom[i].lsf_functional = lsms::LSFFunctional(
             lsms.temperature, lsms::LSFTypeMap[lsf_functional]);
 

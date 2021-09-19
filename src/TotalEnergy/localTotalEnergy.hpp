a31f1fd851c1ccde07f931128cef984d9187d10a
Franco Moitzi <franco.moitzi@mcl.at>
2021-09-19 03:11:51 +0200

    feature(cmake): fix some imports

---
+++ src/TotalEnergy/localTotalEnergy.hpp
@@ -0,0 +1,18 @@
+
+#ifndef MUST_LOCALTOTALENERGY_HPP
+#define MUST_LOCALTOTALENERGY_HPP
+
+#include "Main/SystemParameters.hpp"
+#include "energyStruct.hpp"
+
+extern "C"
+{
+void zeropt_(Real *ezpt, Real *tpzpt, Real *atvol, Real *ztotss);
+}
+
+void
+localTotalEnergy(LSMSSystemParameters &lsms, AtomData &atom, Real &energy, Real &pressure);
+
+
+
+#endif //MUST_LOCALTOTALENERGY_HPP

e52065729e415c1394cc8e42b7081cef6e31ed5e
Franco Moitzi <franco.moitzi@mcl.at>
2021-10-10 01:15:38 +0200

    feature(Misc): add integrator routine

--- src/Misc/integrator.hpp
+++ src/Misc/integrator.hpp
@@ -5,6 +5,9 @@
 #ifndef LSMS_INTEGRATOR_HPP
 #define LSMS_INTEGRATOR_HPP
 
+#include <algorithm>
+#include <functional>
+
 #include "utils.hpp"
 
 namespace lsms {
@@ -39,7 +42,7 @@ namespace lsms {
                 / (6 * h[i] * h[i - 1]);
 
       result += f[i - 1] * (2.0 * lsms::pow(h[i - 1], 3) - lsms::pow(h[i], 3)
-                            + 3. * h[i] * lsms::pow(h[i - 1], 2))
+                            + 3.0 * h[i] * lsms::pow(h[i - 1], 2))
                 / (6 * h[i - 1] * hph);
 
       result += f[i + 1] * (2.0 * lsms::pow(h[i], 3) - lsms::pow(h[i - 1], 3)
@@ -48,27 +51,103 @@ namespace lsms {
 
     }
 
+
     if ((N + 1) % 2 == 0) {
 
-      result += f[N] * (2 * lsms::pow(h[N - 1], 2)
-                        + 3.0 * h[N - 2] * h[N - 1])
-                / (6 * (h[N - 2] + h[N - 1]));
+      if (N > 1) {
 
-      result += f[N - 1] * (lsms::pow(h[N - 1], 2)
-                            + 3.0 * h[N - 1] * h[N - 2])
-                / (6 * h[N - 2]);
+        result += f[N] * (2 * lsms::pow(h[N - 1], 2)
+                          + 3.0 * h[N - 2] * h[N - 1])
+                  / (6 * (h[N - 2] + h[N - 1]));
 
-      result -= f[N - 2] * lsms::pow(h[N - 1], 3)
-                / (6 * h[N - 2] * (h[N - 2] + h[N - 1]));
+        result += f[N - 1] * (lsms::pow(h[N - 1], 2)
+                              + 3.0 * h[N - 1] * h[N - 2])
+                  / (6 * h[N - 2]);
+
+        result -= f[N - 2] * lsms::pow(h[N - 1], 3)
+                  / (6 * h[N - 2] * (h[N - 2] + h[N - 1]));
+
+      } else {
+
+        result += 0.5 * (f[N] + f[N - 1]) * h[N - 1];
+
+      }
     }
 
+
     return result;
+  }
+
+  /**
+   * Radial integrator for any mesh
+   *
+   * This method doesn't use any interpolations for the last point and assumes an alight mesh.
+   * The mesh should be align to according to the `generateRadialMesh` method
+   */
+  template<typename T>
+  T radialIntegral(const std::vector<T> &integrand,
+                   const std::vector<T> &radial_mesh,
+                   const T r_sphere) {
+
+
+    // Index of element that is less then `r_sphere`
+    auto iter = std::upper_bound(radial_mesh.rbegin(), radial_mesh.rend(), r_sphere, std::greater<T>());
+
+    // Index of element that has radius greater then equal `r_sphere`
+    auto i_after_rs = std::distance(radial_mesh.begin(), iter.base());
+
+    auto end_point = i_after_rs + 1;
+
+    return simpson_nonuniform(radial_mesh, integrand, end_point);
+  }
+
+  /**
+  * Radial integrator for any mesh
+  */
+  template<typename T>
+  T radialIntegral(const std::vector<T> &integrand,
+                   const std::vector<T> &radial_mesh,
+                   int end) {
+
+    auto result = simpson_nonuniform(radial_mesh, integrand, end);
+
+    return result;
+  }
+
+  /**
+   * Radial integrator for mesh with an analytic derivative
+   *
+   * \f[ r = r_0 * exp(i * h) ]\f
+   *
+   * dr / di = r * h
+   *
+   */
+  template<typename T>
+  T radialIntegralDerivMesh(const std::vector<T> &val,
+                            const std::vector<T> &radial_mesh_deriv,
+                            int end) {
+
+    T intval = 0.0;
+    int idx;
+
+    for (idx = 0; idx < end - 2; idx += 2) {
+      intval += 1.0 / 3.0 * (val[idx] * radial_mesh_deriv[idx] +
+                             4 * val[idx + 1] * radial_mesh_deriv[idx + 1] +
+                             val[idx + 2] * radial_mesh_deriv[idx + 2]);
+    };
+
+    if ((end % 2) == 0) {
+      intval +=
+          0.5 * (val[idx] * radial_mesh_deriv[idx] + val[idx + 1] * radial_mesh_deriv[idx + 1]);
+    };
+    return intval;
   }
 
 
   template<typename T>
   T radialIntegral(const std::vector<T> &integrand,
                    const std::vector<T> &radial_mesh,
+                   std::vector<T> &integral,
                    const T r_sphere) {
 
 
@@ -105,7 +184,13 @@ namespace lsms {
 
     auto result = simpson_nonuniform(extended_r_mesh, extended_integrand, extended_length);
 
+    integral[0] = 0.0;
+    for (int i = 1; i < extended_length; ++i) {
+      integral[i] = simpson_nonuniform(extended_r_mesh, extended_integrand, i + 1);
+    }
+
     return result;
+
   }
 
 }

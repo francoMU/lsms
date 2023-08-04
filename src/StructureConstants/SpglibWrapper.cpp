2496bb1834c39c85704df4d8666e09aea6d1e83c
Franco Moitzi <franco.moitzi@mcl.at>
2023-08-04 07:49:46 +0200

    add all headers and otherthings

--- src/StructureConstants/SpglibWrapper.cpp
+++ src/StructureConstants/SpglibWrapper.cpp
@@ -5,4 +5,39 @@
 #include "SpglibWrapper.h"
 
 namespace SC {
+
+
+    SpglibWrapper::SpglibWrapper(const Eigen::Matrix<double, 3, 3> &lattice,
+                                 const Eigen::Matrix<double, Eigen::Dynamic, 3> &coordinates,
+                                 const Eigen::Vector<int, Eigen::Dynamic> &specie,
+                                 double symrec) {
+
+        this->dataset = spg_get_dataset(lat, position, types, num_atom, symprec);
+
+
+    }
+
+    SpglibWrapper::SpglibWrapper(const SpglibWrapper& other) {
+
+    }
+
+    SpglibWrapper& SpglibWrapper::operator=(const SpglibWrapper& rhs) {
+
+    }
+
+    SpglibWrapper::SpglibWrapper(SpglibWrapper&& other) noexcept {
+
+    }
+
+    SpglibWrapper& SpglibWrapper::operator=(SpglibWrapper&& rhs) {
+
+    }
+
+
+
+    SpglibWrapper::~SpglibWrapper() {
+        spg_free_dataset(dataset);
+    }
+
+
 } // SC
--- src/StructureConstants/SpglibWrapper.h
+++ src/StructureConstants/SpglibWrapper.h
@@ -5,12 +5,48 @@
 #ifndef LSMS_SPGLIBWRAPPER_H
 #define LSMS_SPGLIBWRAPPER_H
 
+#include "spglib.h"
+#include "Eigen/Dense"
+
+// https://arne-mertz.de/2018/10/calling-cpp-code-from-c-with-extern-c/
+#ifdef __cplusplus
+extern "C" {
+#include "spglib.h"
+}
+#endif
+
 namespace SC {
+
+    constexpr double SYMPREC_DEFAULT = 1.0e-5;
 
     /**
      * Class that wraps the sglib dataset from c
      */
     class SpglibWrapper {
+
+    private:
+
+        double symprec;
+
+        SpglibDataset *dataset;
+
+    public:
+
+        SpglibWrapper(const Eigen::Matrix<double, 3, 3> &lattice,
+                      const Eigen::Matrix<double, Eigen::Dynamic, 3> &coordinates,
+                      const Eigen::Vector<int, Eigen::Dynamic> &specie,
+                      double symprec = SYMPREC_DEFAULT);
+
+        SpglibWrapper(const SpglibWrapper &other);
+
+        SpglibWrapper &operator=(const SpglibWrapper &rhs);
+
+        SpglibWrapper(SpglibWrapper &&other) noexcept;
+
+        SpglibWrapper &operator=(SpglibWrapper &&rhs);
+
+        ~SpglibWrapper();
+
 
     };
 
